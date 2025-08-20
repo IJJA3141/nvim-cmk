c20343b52a4ea022a92b447a53ce36d120952897
IJJA3141 <alex.estella.cj@gmail.com>
2025-08-20 16:21:00 +0200

    06.01 clean up

--- lua/nvim-cmk/functions.lua
+++ lua/nvim-cmk/functions.lua
@@ -1,18 +1,13 @@
 local config = require 'nvim-cmk.config'
 local ui = require 'nvim-cmk.ui'
 
-
-local function is_in(build_type)
-  for _, type in ipairs(config.BUILD_TYPES) do if build_type == type then return end end
-  error(build_type .. " wasn't in the list of valid build types see cmk.config.build_types for more")
-end
-
 local function generate(callback)
   vim.system(
     { "cmake",
       "-DCMAKE_BUILD_TYPE=" .. config.build_type,
       "-S", ".",
-      "-B", config.build_dir .. "/" .. config.build_type },
+      "-B", config.build_dir .. "/" .. config.build_type
+    },
     { cwd = config.cwd, stdout = ui.insert },
     function(result)
       if result.code == 0 then
@@ -24,8 +19,8 @@ local function generate(callback)
           ui.close()
         end
       else
+        ui.insert(result.stderr, result.stdout)
         ui.release()
-        error("failed to generate build files")
       end
     end
   )
@@ -40,83 +35,107 @@ local function build(callback)
         if callback then
           callback.fn(callback.param)
         else
-          print("build succesfully")
+          print("succesfully builed")
           ui.release()
           ui.close()
         end
       else
+        ui.insert(result.stderr, result.stdout)
         ui.release()
-        error("failed to build")
       end
     end
   )
 end
 
-local function test_all()
+local function test_all(callback)
   vim.system(
-    { "ctest", "--test-dir", config.build_dir .. "/" .. config.build_type, "--output-on-failure" },
+    {
+      "ctest", "--output-on-failure",
+      "--test-dir", config.build_dir .. "/" .. config.build_type,
+    },
     { cwd = config.cwd, stdout = ui.insert },
     function(result)
-      ui.release()
-
       if result.code == 0 then
-        print("all tests passed")
-        ui.close()
+        if callback then
+          callback.fn(callback.param)
+        else
+          print("succesfully builed")
+          ui.release()
+          ui.close()
+        end
       else
-        error("a test failed")
+        ui.insert(result.stderr, result.stdout)
+        ui.release()
       end
     end
   )
 end
 
-local function test()
+local function test(callback)
   vim.system(
-    { "ctest", "--test-dir", config.build_dir .. "/" .. config.build_type, "-VV", vim.fn.expand("%:t:r") },
+    {
+      "ctest", "-VV",
+      "--test-dir", config.build_dir .. "/" .. config.build_type,
+      vim.fn.expand("%:t:r")
+    },
     { cwd = config.cwd, stdout = ui.insert },
     function(result)
-      ui.release()
-
       if result.code == 0 then
-        print(vim.fn.expand("%:t:r") .. " tests passed")
-        ui.close()
+        if callback then
+          callback.fn(callback.param)
+        else
+          print("succesfully builed")
+          ui.release()
+          ui.close()
+        end
       else
-        error(vim.fn.expand("%:t:r") .. " failed")
+        ui.insert(result.stderr, result.stdout)
+        ui.release()
       end
     end
   )
+end
+
+local function dap()
+  vim.schedule(function()
+    config.dap.args = { vim.fn.expand("%:t:r") }
+    require("dap").run(config.dap)
+  end)
 end
 
 local M = {}
 
 function M.set_build_type(opts)
-  if opts.args ~= "" then is_in(opts.args)
-    print("settings build_type to " .. opts.args)
-    config.build_type = opts.args
+  if opts.args ~= "" then
+    for _, type in ipairs(config.BUILD_TYPES) do
+      if opts.args == type then
+        print("settings build_type to " .. opts.args)
+        config.build_type = opts.args
+
+        return
+      end
+    end
+
+    error(opts.args .. " wasn't in the list of valid build types see cmk.config.build_types for more")
   end
 end
 
 function M.generate(opts)
-  if opts.args ~= "" then is_in(opts.args) end
   if ui.is_busy() then error("a nvim-cmk prosses is already running") end
-
-  if opts.args ~= "" then config.build_type = opts.args end
+  M.set_build_type(opts)
   vim.cmd("wa")
 
   ui.get()
-
   generate()
 end
 
 function M.build(opts)
-  if opts.args ~= "" then is_in(opts.args) end
   if ui.is_busy() then error("a nvim-cmk prosses is already running") end
-
-  if opts.args ~= "" then config.build_type = opts.args end
+  M.set_build_type(opts)
+  vim.cmd("wa")
 
   ui.get()
-
   if vim.fn.isdirectory(config.cwd .. "/" .. config.build_dir .. "/" .. config.build_type) == 1 then
-    vim.cmd("wa") -- save all buffers
     build()
   else
     generate({ fn = build })
@@ -124,8 +143,6 @@ function M.build(opts)
 end
 
 function M.clean()
-  print("test" .. config.cwd)
-
   vim.system(
     { "rm", "-rf", config.build_dir, "compile_commands.json" },
     { cwd = config.cwd }
@@ -133,15 +150,12 @@ function M.clean()
 end
 
 function M.test_all(opts)
-  if opts.args~="" then is_in(opts.args) end
   if ui.is_busy() then error("a nvim-cmk prosses is already running") end
-
-  if opts.args ~= "" then config.build_type = opts.args end
+  M.set_build_type(opts)
+  vim.cmd("wa")
 
   ui.get()
-
   if vim.fn.isdirectory(config.cwd .. "/" .. config.build_dir .. "/" .. config.build_type) then
-    vim.cmd("wa") -- save all buffers
     build({ fn = test_all })
   else
     generate({ fn = build, param = { fn = test_all } })
@@ -149,37 +163,25 @@ function M.test_all(opts)
 end
 
 function M.test(opts)
-  if opts.args~="" then is_in(opts.args) end
   if ui.is_busy() then error("a nvim-cmk prosses is already running") end
-
-  if opts.args ~= "" then config.build_type = opts.args end
-
+  M.set_build_type(opts)
+  vim.cmd("wa")
 
   ui.get()
   if vim.fn.isdirectory(config.cwd .. "/" .. config.build_dir .. "/" .. config.build_type) then
-    vim.cmd("wa") -- save all buffers
     build({ fn = test })
   else
     generate({ fn = build, param = { fn = test } })
   end
 end
 
-function M.dap()
+function M.dap(opts)
   if ui.is_busy() then error("a nvim-cmk prosses is already running") end
+  M.set_build_type(opts)
+  vim.cmd("wa")
 
-  local function dap()
-    vim.schedule(function()
-      local dap_config = config.dap
-      local name = vim.fn.expand("%:t:r")
-
-      dap_config.args = { name }
-
-      require("dap").run(dap_config)
-    end)
-  end
-
+  ui.get()
   if vim.fn.isdirectory(config.cwd .. "/" .. config.build_dir .. "/" .. config.build_type) then
-    vim.cmd("wa") -- save all buffers
     build({ fn = dap })
   else
     generate({ fn = build, param = { fn = dap } })
--- lua/nvim-cmk/init.lua
+++ lua/nvim-cmk/init.lua
@@ -18,42 +18,52 @@ function cmk.setup(opts)
   cmk.test_all = functions.test_all
   cmk.test = functions.test
   cmk.dap = functions.dap
+
   cmk.clean = functions.clean
 
-  print(vim.inspect(config.BUILD_TYPES))
+  local completion = function(_, _, _) return config.BUILD_TYPES end
 
   if config.register_autocmd then
     vim.api.nvim_create_user_command("CMakeShow", cmk.show, { desc = "Show nvim-cmk popup" })
     vim.api.nvim_create_user_command("CMakeHide", cmk.hide, { desc = "Hide nvim-cmk popup" })
     vim.api.nvim_create_user_command("CMakeToggle", cmk.toggle, { desc = "Toggle nvim-cmk popup" })
 
-    vim.api.nvim_create_user_command("CMakeSetBuildType", cmk.set_build_type , {
-        desc = "Set build type",
-        nargs = "?",
-        complete = function(_, _, _) return config.BUILD_TYPES end,
-      })
-
-    vim.api.nvim_create_user_command("CMakeGenerate", cmk.generate,
-      {
-        desc = "generate",
-        nargs = "?",
-        complete = function(_, _, _) return config.BUILD_TYPES end,
-      })
-
-    vim.api.nvim_create_user_command("CMakeBuild", cmk.build,
-    { desc = "Build" , nargs = "?", complete = function(_, _, _) return config.BUILD_TYPES end, })
-
-    vim.api.nvim_create_user_command("CMakeTestAll", cmk.test_all, { desc = "Run all tests" 
-, nargs = "?", complete = function(_, _, _) return config.BUILD_TYPES end,
-  })
-
-    vim.api.nvim_create_user_command("CMakeTest",
-      function(args) if args.args then cmk.test(args.args) end end,
-      { desc = "Run a test"
-, nargs = "?", complete = function(_, _, _) return config.BUILD_TYPES end,
+    vim.api.nvim_create_user_command("CMakeSetBuildType", cmk.set_build_type, {
+      desc = "Set build type",
+      nargs = "?",
+      complete = completion
     })
 
-    vim.api.nvim_create_user_command("CMakeDap", cmk.dap, { desc = "Dap" })
+    vim.api.nvim_create_user_command("CMakeGenerate", cmk.generate, {
+      desc = "Generate",
+      nargs = "?",
+      complete = completion
+    })
+
+    vim.api.nvim_create_user_command("CMakeBuild", cmk.build, {
+      desc = "Build",
+      nargs = "?",
+      complete = completion
+    })
+
+    vim.api.nvim_create_user_command("CMakeTestAll", cmk.test_all, {
+      desc = "Run all tests",
+      nargs = "?",
+      complete = completion
+    })
+
+    vim.api.nvim_create_user_command("CMakeTest", cmk.test, {
+      desc = "Test",
+      nargs = "?",
+      complete = completion
+    })
+
+    vim.api.nvim_create_user_command("CMakeDap", cmk.dap, {
+      desc = "Dap",
+      nargs = "?",
+      complete = completion
+    })
+
     vim.api.nvim_create_user_command("CMakeClean", cmk.clean, { desc = "Clean" })
   end
 end
